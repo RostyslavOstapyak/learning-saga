e0757de77632468ae53a1d11a32619eabc6a2feb
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-12-24 15:49:02 +0200

    aded takeEvery

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,15 +1,16 @@
-import { take } from "redux-saga/effects";
+import { take, takeEvery } from "redux-saga/effects";
 
 export function* workerSaga() {
     console.log('click from saga');
 }
 
 export function* watchClickSaga() {
-    while (true) {
-        yield take('CLICK');
+    // while (true) {
+    //     yield take('CLICK');
 
-        yield workerSaga();
-    }
+    //     yield workerSaga();
+    // }
+    yield takeEvery('CLICK', workerSaga);
 }
 
 export default function* rootSaga() {
